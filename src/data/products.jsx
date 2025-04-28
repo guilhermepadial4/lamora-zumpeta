df27ad197d885aa9a50c5da554e1ea2c6c2f19d4
Guilherme Padial <guilherme.padial4@gmail.com>
2025-04-28 12:06:15 -0300

    update: project

--- src/data/products.jsx
+++ src/data/products.jsx
@@ -3,6 +3,14 @@ import Edge from '../assets/img/edge.png';
 import ZenithPlus from '../assets/img/zenithp.png';
 import ZenithX from '../assets/img/zenithx.png';
 import CoreS from '../assets/img/core-s.png';
+import TabM10 from '../assets/img/tabm10.png';
+import TabX12Pro from '../assets/img/tabx12pro.png';
+import BookE14 from '../assets/img/booke14.png';
+import BookM15 from '../assets/img/bookm15.png';
+import CoreX from '../assets/img/corex.png';
+import Sound from '../assets/img/sound.png';
+import Buds from '../assets/img/buds.png';
+import Power from '../assets/img/power.png';
 
 export const products = [
   {
@@ -48,5 +56,77 @@ export const products = [
     price: '1899',
     image: CoreS,
     slug: 'lumora-core-s',
+  },
+
+  {
+    id: 6,
+    name: 'Lumora Tab M10',
+    description: 'Equilíbrio perfeito entre performance, conectividade e estilo.',
+    price: '1899',
+    image: TabM10,
+    slug: 'lumora-tab-m10',
+  },
+
+  {
+    id: 7,
+    name: 'Lumora Tab X12 Pro',
+    description: 'Equilíbrio perfeito entre performance, conectividade e estilo.',
+    price: '3699',
+    image: TabX12Pro,
+    slug: 'lumora-tab-x12-pro',
+  },
+
+  {
+    id: 8,
+    name: 'Lumora Book E14',
+    description: 'Equilíbrio perfeito entre performance, conectividade e estilo.',
+    price: '3599',
+    image: BookE14,
+    slug: 'lumora-book-e14',
+  },
+
+  {
+    id: 9,
+    name: 'Lumora Book M15',
+    description: 'Equilíbrio perfeito entre performance, conectividade e estilo.',
+    price: '4899',
+    image: BookM15,
+    slug: 'lumora-book-m15',
+  },
+
+  {
+    id: 10,
+    name: 'Lumora Core X',
+    description: 'Equilíbrio perfeito entre performance, conectividade e estilo.',
+    price: '6299',
+    image: CoreX,
+    slug: 'lumora-core-x',
+  },
+
+  {
+    id: 11,
+    name: 'Headphone Lumora SoundWave X',
+    description: 'Equilíbrio perfeito entre performance, conectividade e estilo.',
+    price: '1199',
+    image: Sound,
+    slug: 'headphone-lumora-soundwave-x',
+  },
+  
+  {
+    id: 12,
+    name: 'SoundBuds Air',
+    description: 'Equilíbrio perfeito entre performance, conectividade e estilo.',
+    price: '1699',
+    image: Buds,
+    slug: 'soundbuds-air',
+  },
+
+  {
+    id: 13,
+    name: 'PowerMini 5000',
+    description: 'Equilíbrio perfeito entre performance, conectividade e estilo.',
+    price: '1399',
+    image: Power,
+    slug: 'powermini-5000',
   },
 ];
