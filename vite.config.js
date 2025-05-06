deb38d67ccdca294e25fc5f8c35b80d46174d0d7
Guilherme Padial <guilherme.padial4@gmail.com>
2025-05-06 15:09:00 -0300

    update: vite

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    visualizer({ open: true }), 
-  ],
+  plugins: [react()],
   build: {
     minify: 'esbuild',
     sourcemap: false,
